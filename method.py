6ff30fcf861976909a046b6066baa5c647fc00f9
candle <progted@gmail.com>
2015-01-10 17:28:24 +0900

    OS判定用の関数を作成

--- method.py
+++ method.py
@@ -2,14 +2,15 @@
 import base64
 SHELL_ESCAPE= " '\";`|"
 
-def get_install_command():
+def check_os():
 	if command_check("apt-get"):
-		tmp_command='apt-get install '
+		return 'ubuntu'
 	elif command_check("yum"):
-		tmp_command='yum install '
+		return 'centos'
 	elif command_check("brew"):
-		tmp_command='brew install '
-	return tmp_command
+		return 'mac'
+
+
 
 def shell_safe(path):
 	return "".join([("\\" + _) if _ in SHELL_ESCAPE else _ for _ in path])
