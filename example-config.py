2ba7b4d9365a2e45a294f00f867ced00fa755d36
candle <progted@gmail.com>
2015-02-07 14:59:33 +0900

    example-configの設定を変更

--- example-config.py
+++ example-config.py
@@ -1,24 +1,27 @@
+# -*- encoding:utf-8 -*-
+from fabric.api import env, settings,local,run,sudo,cd
+from fabric.colors import *
+from fabric.decorators import task,roles
+from fabric.contrib.files import *
+from cuisine import mode_sudo, select_package,package_ensure, dir_exists,file_append,file_exists,command_check
+
+#使用するグローバル変数
+EMACS_FILE = '~/.emacs.d/init.el'
+EMACS_DIR = '~/.emacs.d/'
+EMACS_CONF = '~/.emacs.d/conf/'
+EMACS_ELISP_DIR='~/.emacs.d/elisp/'
+
 #サーバのIPアドレス。
+#グローバルでやる場合はグローバルIPアドレス
 #ローカルでやる場合はプライベートIPアドレス
 env.hosts = ['xxx.xxx.xxx.xxx']
 env.user = "your_pc_name"
 
 #パスワード認証の場合はそのPCのパスワードを
 env.password ="your_pc_password"
+
 #公開鍵認証の場合は秘密鍵のパス
 #env.key_filename = '/path/to/secretkeyfile.pem'
-
-#サーバのIPアドレス。
-#グローバルでやる場合はグローバルIPアドレス
-#ローカルでやる場合はプライベートIPアドレス
-env.hosts = ['192.168.1.1']
-env.user = "candle"
-
-#パスワード認証の場合はそのPCのパスワードを
-env.password ="hogehoge"
-
-#公開鍵認証の場合は秘密鍵のパス
-#env.key_filename = '~/.ssh/hoge.pem'
 
 #vagrant環境でやる場合は下をコメントイン
 #env.key_filename = '~/.vagrant.d/insecure_private_key'
