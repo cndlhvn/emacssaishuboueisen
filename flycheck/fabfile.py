09406afea364d39ff90b4d65cff5f21a8b493b9e
candle <progted@gmail.com>
2015-02-19 17:57:20 +0900

    flycheckの自動インストールのfabfile.pyを作成

---
+++ flycheck/fabfile.py
@@ -0,0 +1,17 @@
+# -*- encoding:utf-8 -*-
+
+execfile("../config.py")
+execfile("../method.py")
+
+@task
+def flycheck_installing():
+  """[1]flycheckのインストール"""
+  with cd(EMACS_CONF):
+    if not contains("elpa-component.el",";;;flycheck"):
+      register_library_to_elpa("flycheck")
+      run("emacs --batch --script elpa-component.el")
+
+  if not contains(EMACS_FILE,";;;flycheck"):
+    file_appendln(EMACS_FILE,";;;flycheck")
+    file_appendln(EMACS_FILE,"(add-hook 'after-init-hook #'global-flycheck-mode)")
+    file_appendln(EMACS_FILE,"")
