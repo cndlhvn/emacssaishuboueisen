8166f35e5135ca285e9aa4edae0cc2f462236726
candle <progted@gmail.com>
2015-06-05 17:39:36 +0900

    anythingのappendlnを複数行に。anything-killring-listを導入できるようにした。

--- anything/fabfile.py
+++ anything/fabfile.py
@@ -12,36 +12,45 @@ def anything_install():
       run("emacs --batch --script elpa-component.el")
       
   if not contains(EMACS_FILE,";;;anything"):
-    file_appendln(EMACS_FILE,";;;anything")
-    file_appendln(EMACS_FILE,"(when (require 'anything nil t)")
-    file_appendln(EMACS_FILE,"(when (require 'anything-startup nil t))",1)
-    file_appendln(EMACS_FILE,"(setq",1)
-    file_appendln(EMACS_FILE,";; 候補を表示するまでの時間。デフォは0.5",2)
-    file_appendln(EMACS_FILE,"anything-idle-delay 0.3",2)
-    file_appendln(EMACS_FILE,";; タイプして再描写するまでの時間。デフォルトは0.1",2)
-    file_appendln(EMACS_FILE,"anything-input-idle-delay 0.2",2)
-    file_appendln(EMACS_FILE,";; 最近使ったファイル候補の最大表示数。デフォルトは50",2)
-    file_appendln(EMACS_FILE,"anything-candidate-number-limit 10",2)
-    file_appendln(EMACS_FILE,";; 候補が多いときに体感速度を早くする",2)
-    file_appendln(EMACS_FILE,"anything-quick-update t",2)
-    file_appendln(EMACS_FILE,";; 候補選択ショートカットをアルファベットに",2)
-    file_appendln(EMACS_FILE,"anything-enable-shortcuts 'alphabet)",2)
-    file_appendln(EMACS_FILE,";; C-x C-aでanythingを起動",2)
-    file_appendln(EMACS_FILE,'(define-key global-map (kbd "C-x C-a") \'anything)',1)
-    file_appendln(EMACS_FILE,'')
-    file_appendln(EMACS_FILE,"(when (require 'anything-config nil t)",1)
-    file_appendln(EMACS_FILE,";;root権限でアクションを実行するときのコマンド",2)
-    file_appendln(EMACS_FILE,';; デフォルトは"su"',2)
-    file_appendln(EMACS_FILE,';;anythingで表示されたファイルリストにカーソルを当てたまま',2)
-    file_appendln(EMACS_FILE,';;タブを押すとファイルに対してのアクションが選択できる',2)
-    file_appendln(EMACS_FILE,';;そこでFind file as rootを選んでファイルを開く時、sudoでファイルを開く',2)
-    file_appendln(EMACS_FILE,'(setq anything-su-or-sudo "sudo"))',2)
-    file_appendln(EMACS_FILE,'')
-    file_appendln(EMACS_FILE,';;今のところこの下の設定は使い方が分からない',1)
-    file_appendln(EMACS_FILE,"(require 'anything-show-completion nil t)",1)
-    file_appendln(EMACS_FILE,"(when (require 'anything-complete nil t)",1)
-    file_appendln(EMACS_FILE,";; lispシンボルの補完候補の再検索時間",2)
-    file_appendln(EMACS_FILE,"(anything-lisp-complete-symbol-set-timer 150)))",2)
-    file_appendln(EMACS_FILE,'')
-    
+    anything="""
+;;;anything    
+  (when (require 'anything nil t)
+  (when (require 'anything-startup nil t))
+  (setq
+   ;; 候補を表示するまでの時間。デフォは0.5
+   anything-idle-delay 0.3
+   ;; タイプして再描写するまでの時間。デフォルトは0.1
+   anything-input-idle-delay 0.2
+   ;; 最近使ったファイル候補の最大表示数。デフォルトは50
+   anything-candidate-number-limit 10
+   ;; 候補が多いときに体感速度を早くする
+   anything-quick-update t
+   ;; 候補選択ショートカットをアルファベットに
+   anything-enable-shortcuts 'alphabet)
+  ;; C-x C-aでanythingを起動
+  (define-key global-map (kbd "C-x C-a") \'anything)
 
+  (when (require 'anything-config nil t)
+    ;;root権限でアクションを実行するときのコマンド
+    ;; デフォルトは"su "
+    ;;anythingで表示されたファイルリストにカーソルを当てたまま
+    ;;タブを押すとファイルに対してのアクションが選択できる
+    ;;そこでFind file as rootを選んでファイルを開く時、sudoでファイルを開く
+    (setq anything-su-or-sudo "sudo"))
+  ;;今のところこの下の設定は使い方が分からない
+  (require 'anything-show-completion nil t)
+  (when (require 'anything-complete nil t)
+    ;; lispシンボルの補完候補の再検索時間
+    (anything-lisp-complete-symbol-set-timer 150)))"""
+    file_appendln(EMACS_FILE,anything)
+    
+@task
+def anything_killring_list():
+  """[2]anythingのkill-ring-listの設定"""
+  if not contains(EMACS_FILE,";;;anything_killring"):
+    anything_killring="""
+;;;anything_killring
+;;コピーやカットの履歴から貼り付けが可能
+(setq anything-kill-ring-threshold 5)
+(global-set-key "\M-y" 'anything-show-kill-ring)"""
+    file_appendln(EMACS_FILE,anything_killring)
