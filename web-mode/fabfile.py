33203cfb61d3361026b385273f739862c037d4f4
candle <progted@gmail.com>
2015-06-05 18:58:27 +0900

    webモードを複数行記述に変更

--- web-mode/fabfile.py
+++ web-mode/fabfile.py
@@ -12,34 +12,40 @@ def web_mode_install():
       run("emacs --batch --script elpa-component.el")
 
   if not contains(EMACS_FILE,";;;web_mode"):
-    file_appendln(EMACS_FILE,";;;web_mode")
-    file_appendln(EMACS_FILE,"(when (require 'web-mode nil t)")
-    file_appendln(EMACS_FILE,";;web-modeを適応させる拡張子",1)
-    file_appendln(EMACS_FILE,"(add-to-list 'auto-mode-alist '(\"\.php\" . web-mode))",1)
-    file_appendln(EMACS_FILE,"(add-to-list 'auto-mode-alist '(\"\.html\". web-mode))",1)
-    file_appendln(EMACS_FILE,"(add-to-list 'auto-mode-alist '(\"\.html.erb\". web-mode))",1)
-    file_appendln(EMACS_FILE,"(add-to-list 'auto-mode-alist '(\"\.ctp\" . web-mode))",1)
-    file_appendln(EMACS_FILE,"(defun my-web-mode-hook ()",1)
-    file_appendln(EMACS_FILE,";;マークアップのタブ幅をスペース２個",1)
-    file_appendln(EMACS_FILE,"(setq web-mode-markup-indent-offset 2)",2)
-    file_appendln(EMACS_FILE,";;スタイシートでタブ幅をスペース２個",1)
-    file_appendln(EMACS_FILE,"(setq web-mode-css-indent-offset 2)",2)
-    file_appendln(EMACS_FILE,";;プログラムでタブ幅をスペース２個",1)
-    file_appendln(EMACS_FILE,"(setq web-mode-code-indent-offset 2))",2)
-    file_appendln(EMACS_FILE,"(add-hook 'web-mode-hook  'my-web-mode-hook))",1)
-    file_appendln(EMACS_FILE,"")
+    web_mode="""
+;;;web_mode
+(when (require 'web-mode nil t)
+  ;;web-modeを適応させる拡張子
+  (add-to-list 'auto-mode-alist '(\"\.php\" . web-mode))
+  (add-to-list 'auto-mode-alist '(\"\.html\". web-mode))
+  (add-to-list 'auto-mode-alist '(\"\.html.erb\". web-mode))
+  (add-to-list 'auto-mode-alist '(\"\.ctp\" . web-mode))
+  (defun my-web-mode-hook ()
+    ;;インデントモードをオフ
+    (setq-default indent-tabs-mode nil)
+    ;;タブ幅をスペース２つ
+    (setq tab-width 2)
+    ;;マークアップのタブ幅をスペース２個
+    (setq web-mode-markup-indent-offset 2)
+    ;;スタイシートでタブ幅をスペース２個
+    (setq web-mode-css-indent-offset 2)
+    (setq css-indent-offset 2)
+    ;;プログラムでタブ幅をスペース２個
+    (setq web-mode-code-indent-offset 2))
+  (add-hook 'web-mode-hook  'my-web-mode-hook))"""
+    file_appendln(EMACS_FILE,web_mode)
 
 @task
 def web_mode_color():
   """[2]web-modeでのhtmlの色を設定"""
   if not contains(EMACS_FILE,";;;web_mode_color"):
-    file_appendln(EMACS_FILE,";;;web_mode_color")
-    file_appendln(EMACS_FILE,";;端末の背景が黒系の時に見やすい色に設定")
-    file_appendln(EMACS_FILE,"(set-face-attribute 'web-mode-doctype-face nil :foreground \"Blue\")",1)
-    file_appendln(EMACS_FILE,"(set-face-attribute 'web-mode-html-tag-face nil :foreground \"White\")",1)
-    file_appendln(EMACS_FILE,"(set-face-attribute 'web-mode-html-tag-bracket-face nil :foreground \"White3\")",1)
-    file_appendln(EMACS_FILE,"(set-face-attribute 'web-mode-html-attr-name-face nil :foreground \"Red\")",1)
-    file_appendln(EMACS_FILE,"(set-face-attribute 'web-mode-html-attr-value-face nil :foreground \"Yellow\")",1)
-    file_appendln(EMACS_FILE,"")
-
-
+    web_mode_color="""
+;;;web_mode_color
+;;端末の背景が黒系の時に見やすい色に設定
+(set-face-attribute 'web-mode-doctype-face nil :foreground \"Blue\")
+(set-face-attribute 'web-mode-html-tag-face nil :foreground \"White\")
+(set-face-attribute 'web-mode-html-tag-bracket-face nil :foreground \"White3\")
+(set-face-attribute 'web-mode-html-attr-name-face nil :foreground \"Red\")
+(set-face-attribute 'web-mode-html-attr-value-face nil :foreground \"Yellow\")
+"""
+    file_appendln(EMACS_FILE,web_mode_color)
