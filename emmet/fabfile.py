8532909c6dd152b97bf8941a0f3594277079037c
candle <progted@gmail.com>
2015-06-05 18:58:49 +0900

    emmetのインデントを揃える

--- emmet/fabfile.py
+++ emmet/fabfile.py
@@ -16,14 +16,14 @@ def emmet_install():
 ;;;emmet-mode
 ;;emmetの記法の文末でC-jで展開
 (when (require 'emmet-mode nil t)
-;; マークアップ言語全部で使う
-(add-hook 'sgml-mode-hook 'emmet-mode)
-;; CSSにも使う
-(add-hook 'css-mode-hook  'emmet-mode)
-;; web-modeに付け加える
-(add-hook 'web-mode-hook  'emmet-mode)
-;; php-modeに付け加える
-(add-hook 'php-mode-hook 'emmet-mode)
-;; indent はスペース2個
-(add-hook 'emmet-mode-hook (lambda () (setq emmet-indentation 2))))"""
+  ;; マークアップ言語全部で使う
+  (add-hook 'sgml-mode-hook 'emmet-mode)
+  ;; CSSにも使う
+  (add-hook 'css-mode-hook  'emmet-mode)
+  ;; web-modeに付け加える
+  (add-hook 'web-mode-hook  'emmet-mode)
+  ;; php-modeに付け加える
+  (add-hook 'php-mode-hook 'emmet-mode)
+  ;; indent はスペース2個
+  (add-hook 'emmet-mode-hook (lambda () (setq emmet-indentation 2))))"""
     file_appendln(EMACS_FILE,emmet_mode)
