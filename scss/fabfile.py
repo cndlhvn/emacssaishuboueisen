2307c07c6c29b1b44bec755cc019d7f42c15ffd1
candle <progted@gmail.com>
2015-06-19 13:04:45 +0900

    sassのインストール

---
+++ scss/fabfile.py
@@ -0,0 +1,30 @@
+# -*- encoding:utf-8 -*-
+
+execfile("../config.py")
+execfile("../method.py")
+
+@task
+def scss_install():
+  """[1]scssのインストール"""
+  with cd(EMACS_CONF):
+    if not contains("elpa-component.el",";;;scss-mode"):
+      register_library_to_elpa("scss-mode")
+      run("emacs --batch --script elpa-component.el")
+
+  if not contains(EMACS_FILE,";;;scss-mode"):
+    scss_mode="""
+;;;scss-mode
+(when (require 'scss-mode nil t)
+(add-to-list 'auto-mode-alist '("\.scss$" . scss-mode))
+;; インデント幅を2にする
+;; コンパイルは compass watchで行うので自動コンパイルをオフ
+(defun scss-custom ()
+  ;;インデントモードをオフ
+  (setq-default indent-tabs-mode nil)
+  ;;タブの幅をスペース２つに設定
+  (set (make-local-variable 'css-indent-offset) 2)
+  (setq tab-width 2)
+  ;; コンパイルは compass watchで行うので自動コンパイルをオフ
+  (set (make-local-variable 'scss-compile-at-save) nil))
+(add-hook 'scss-mode-hook 'scss-custom))"""
+    file_appendln(EMACS_FILE,scss_mode)
