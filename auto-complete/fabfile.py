94eb86ee6f12d648a44f1e89c8314843f158636c
candle <progted@gmail.com>
2015-10-11 17:24:30 +0900

    auto-completeをelopeからel-getへ

--- auto-complete/fabfile.py
+++ auto-complete/fabfile.py
@@ -6,22 +6,14 @@ execfile("../method.py")
 @task
 def auto_complete_installing():
   """[1]auto-completeのインストール"""
-  with cd(EMACS_CONF):
-    if not contains("elpa-component.el",";;;auto-complete"):
-      register_library_to_elpa("auto-complete")
-      run("emacs --batch --script elpa-component.el")
-
-    if not dir_exists(EMACS_DIR+"ac-dict"):
-      run("mkdir "+EMACS_DIR+"ac-dict")
-      
-    if not contains(EMACS_FILE,";;;auto-complete"):
-      auto_complete="""
+  if not contains(EMACS_FILE,";;;auto-complete"):
+    auto_complete="""
 ;;;auto-complete
+(el-get-bundle auto-complete)
 (when (require 'auto-complete-config nil t)
   (ac-config-default)
-  ;;auto-completeの候補データを収めるフォルダを指定
-  (add-to-list 'ac-dictionary-directories "~/.emacs.d/ac-dict")
   ;;M-tabで補完候補を表示
   (define-key ac-mode-map (kbd "M-TAB") 'auto-complete))"""
-      file_appendln(EMACS_FILE,auto_complete)
-
+    file_appendln(EMACS_FILE,auto_complete)
+    with cd(EMACS_DIR):
+      run("emacs --batch --script init.el")
