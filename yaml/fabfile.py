73f59c7746e52ccf26307190fc656d4060c6b8ec
candle <progted@gmail.com>
2015-10-11 23:32:16 +0900

    yaml-modeをel-getに変更

--- yaml/fabfile.py
+++ yaml/fabfile.py
@@ -6,15 +6,12 @@ execfile("../method.py")
 @task
 def yaml_install():
   """[1]yamlのインストール"""
-  with cd(EMACS_CONF):
-    if not contains("elpa-component.el",";;;yaml-mode"):
-      register_library_to_elpa("yaml-mode")
-      run("emacs --batch --script elpa-component.el")
-
   if not contains(EMACS_FILE,";;;yaml-mode"):
     yaml_mode="""
 ;;;yaml-mode
+(el-get-bundle yaml-mode)
 (when (require 'yaml-mode nil t)
 (add-to-list 'auto-mode-alist '("\\.yml$" . yaml-mode)))"""
     file_appendln(EMACS_FILE,yaml_mode)
-    
+    with cd(EMACS_DIR):
+      run("emacs --batch --script init.el")
