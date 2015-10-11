246b0e9a83c69cd3ca1f79dbe0665e38313805ef
candle <progted@gmail.com>
2015-10-11 18:25:05 +0900

    dmacroをel-getに対応

--- dmacro/fabfile.py
+++ dmacro/fabfile.py
@@ -6,18 +6,18 @@ execfile("../method.py")
 @task
 def dmacro_install():
   """[1]dmacroのインストール"""
-  with cd(EMACS_CONF):
-    if not contains("elpa-component.el",";;;dmacro"):
-      register_elisp_to_elpa("dmacro",'http://www.pitecan.com/papers/JSSSTDmacro/dmacro.el')
-      run("emacs --batch --script elpa-component.el")
-      
   if not contains(EMACS_FILE,";;;dmacro_setting"):
     dmacro_setting ="""
 ;;;dmacro_setting
 ;;繰り返したい処理をemacs上で2回以上繰り返す
 ;;C-tで繰り返せる
-(defconst *dmacro-key* \"\C-t\" \"繰返し指定キー\")
+(el-get-bundle dmacro
+	:type http
+	:url "http://www.pitecan.com/papers/JSSSTDmacro/dmacro.el")
+(defconst *dmacro-key* "\C-t" "繰返し指定キー")
 (global-set-key *dmacro-key* 'dmacro-exec)
-(autoload 'dmacro-exec \"dmacro\" nil t)"""
+(autoload 'dmacro-exec "dmacro" nil t)"""
     file_appendln(EMACS_FILE,dmacro_setting)
+    with cd(EMACS_DIR):
+      run("emacs --batch --script init.el")
 
