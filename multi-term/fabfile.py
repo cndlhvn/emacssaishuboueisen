4beba8e5d142ab48f74eb12742a74fc8d9016aa3
candle <progted@gmail.com>
2015-10-11 23:27:58 +0900

    multitermの設定をel-getへ移行

--- multi-term/fabfile.py
+++ multi-term/fabfile.py
@@ -6,14 +6,12 @@ execfile("../method.py")
 @task
 def multi_term_install():
   """[1]multi-termのインストール"""
-  with cd(EMACS_CONF):
-    if not contains("elpa-component.el",";;;multi-term"):
-      register_library_to_elpa("multi-term")
-      run("emacs --batch --script elpa-component.el")
-
   if not contains(EMACS_FILE,";;;multi-term"):
     multi_term="""
 ;;;multi-term
+(el-get-bundle multi-term)
 (when (require 'multi-term nil t)
   (setq multi-term-program "/bin/bash"))"""
     file_appendln(EMACS_FILE,multi_term)
+    with cd(EMACS_DIR):
+      run("emacs --batch --script init.el")
