f8d31f5a82e55191fd91febad49235fed1056e5f
candle <progted@gmail.com>
2015-10-11 22:51:38 +0900

    coffee-modeをel-getに移行

--- coffee/fabfile.py
+++ coffee/fabfile.py
@@ -6,21 +6,17 @@ execfile("../method.py")
 @task
 def coffee_install():
   """[1]coffee-modeのインストール"""
-  with cd(EMACS_CONF):
-    if not contains("elpa-component.el",";;;coffee-mode"):
-      register_library_to_elpa("coffee-mode")
-      run("emacs --batch --script elpa-component.el")
-
   if not contains(EMACS_FILE,";;;coffee-mode"):
     coffee_mode="""
 ;;;coffee-mode
+(el-get-bundle coffee-mode :depends (dash))
 (when (require 'coffee-mode nil t)
   (defun coffee-custom ()
     ;;インデントモードをオフ
     (setq-default indent-tabs-mode nil)
     ;;タブの幅をスペース２つに設定
     (setq tab-width 2))
-  (add-hook 'coffee-mode-hook 'coffee-custom))
-"""
+  (add-hook 'coffee-mode-hook 'coffee-custom))"""
     file_appendln(EMACS_FILE,coffee_mode)
-    
+    with cd(EMACS_DIR):
+      run("emacs --batch --script  init.el")
